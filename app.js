9cddbc748fd07a3a9aac1ef2a61f2d8244dcf2a0
Anesti Gjikoka <anesti.gjikoka@gmail.com>
2019-08-25 13:51:42 +0200

    Objects and Objects Literal

--- app.js
+++ app.js
@@ -1,18 +1,41 @@
-var person = new Object();
+// var person = new Object();
 
-person["firstname"] = "Nesti";
-person["lastname"] = "Gjikoka";
+// person["firstname"] = "Nesti";
+// person["lastname"] = "Gjikoka";
 
-var firstNameProperty = "firstname";
+// var firstNameProperty = "firstname";
 
-console.log(person);
-console.log(person.firstname);
-console.log(person.lastname);
+// console.log(person);
+// console.log(person.firstname);
+// console.log(person.lastname);
 
-person.address = new Object();
-person.address.address = "Main Street";
-person.address.city = "New York";
+// person.address = new Object();
+// person.address.address = "Main Street";
+// person.address.city = "New York";
 
 
-console.log(person.address.address);
+// console.log(person.address.address);
 
+
+
+// Objects and Objects Literal
+
+var Nesti = {
+    firstname : "Nesti",
+    lastname : "Gjikoka",
+    address : {
+        street: "Main Street"
+    }
+};
+
+
+function greet(person) {
+    console.log('Hi ' + person.firstname);
+}
+
+greet(Nesti);
+
+
+greet({
+    firstname: 'Maria',
+})
